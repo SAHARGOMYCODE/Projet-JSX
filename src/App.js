5b41b1ac9769edd60cef009071e7590c316f4baa
souroursahar2020 <sahar.kachoukh@gmail.com>
2020-10-16 10:02:13 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <div className="App">
       <h1 class="title red">The legende of ZELDA</h1>
-      <img src={link} /> <br />
-      <img src="image/zelda.jpg" />
+      <img src={link} alt="link" /> <br />
+      <img src="image/zelda.jpg" alt="zelda" />
       <br />
       <audio controls src={sidon} />
     </div>
